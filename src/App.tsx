6cb43b3e776bd68504820e53e8ff1d918fc37a27
murafalizade <hmmuralizade29@gmail.com>
2022-02-07 10:13:24 +0400

    client commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import PredictForm from './components/form'
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='main'>
+        <h3 className='head-title'>Kateqoriyalar üzrə evlərin qiymət hesablanması</h3>
+        <PredictForm />
+      </div>
     </div>
   );
 }
